c5e9c96981a6c4c2058d115987e12bcee70dba7d
Ramprasaddrp <ramprasadd0413@gmail.com>
2023-12-18 22:42:19 +0530

    save changes

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("ram");
+const express = require("express");
+const path = require("path");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const app = express();
+const dbPath = path.join(__dirname, nxtwatch.db);
